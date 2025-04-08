d60029452dd440032e992dab52acd94bfe686073
Hussein <hussein.osman.@student.moringaschool.com>
2025-04-08 12:26:49 +0300

    seve change complited

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
+// src/components/Home.js
 import React from "react";
+import { username, city } from "../data/user";
 
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>{username} is a Web Developer from {city}</h1>
     </div>
   );
 }
+
+export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
+// src/components/NavBar.js
 import React from "react";
 
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
     </nav>
   );
 }
+
+export default NavBar;
